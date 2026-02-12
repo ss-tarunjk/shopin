781f38db480d7df7eb8de90c2133b51bcf622481
Tarun Jaikishan <tarun.jaikishan@surveysparrow.com>
2026-02-12 10:34:41 +0530

    configured redux, query and router

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from "react-redux";
+
+const queryClient = new QueryClient()
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      {/* <Provider> */}
+      <Router>
+        <App />
+      </Router>
+      {/* </Provider> */}
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
