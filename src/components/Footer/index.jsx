20f5e4697663634e990b0c1038b704686dc2ec2a
Tarun Jaikishan <tarun.jaikishan@surveysparrow.com>
2026-02-12 23:59:27 +0530

    optimized footer data

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,9 @@
+import { Link } from "react-router-dom";
 import styles from "./Footer.module.css";
+
+import FOOTER_MAIN_ITEMS from "./data.json";
+
+import appConfig from "../config/app.config";
 
 export default function Footer() {
   return (
@@ -10,7 +15,7 @@ export default function Footer() {
             <h2 className={styles["footer-subscribe-text"]}>
               STAY UPTO DATE ABOUT OUR LATEST OFFERS
             </h2>
-            åß
+
             <div className={styles["footer-subscribe-action"]}>
               <input
                 type="text"
@@ -28,10 +33,39 @@ export default function Footer() {
               </button>
             </div>
           </div>
-        </div>
 
-        {/* Main Footer section */}
-        <div></div>
+          {/* Main Footer section */}
+          <div className={`${styles["footer-main"]} flex justify-between`}>
+            <div className={styles["footer-main-item"]}>
+              <div>
+                <h2>{appConfig.appName}</h2>
+                <p>
+                  We have clothes that suits your style and which you’re proud
+                  to wear. From women to men.
+                </p>
+              </div>
+              <div></div>
+            </div>
+
+            {FOOTER_MAIN_ITEMS.map((item, i) => (
+              <div className={styles["footer-main-item"]} key={i}>
+                <h4 className={styles["footer-main-item-title"]}>
+                  {item.title}
+                </h4>
+
+                {item.links.map((link, i) => (
+                  <Link
+                    to={link.href}
+                    key={i}
+                    className={styles["footer-main-item-link"]}
+                  >
+                    {link.label}
+                  </Link>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </footer>
   );
