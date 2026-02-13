122115193f4df28caf8e6c591479130d8767fb88
Tarun Jaikishan <tarun.jaikishan@surveysparrow.com>
2026-02-13 14:33:47 +0530

    added css to hero text section

--- src/components/home/Hero/index.jsx
+++ src/components/home/Hero/index.jsx
@@ -1,41 +1,71 @@
 import styles from "./Hero.module.css";
 
-const BRAND_IMAGES = ["brand1.png", "brand2.png", "brand3.png", "brand4.png"];
+const HERO_STATS = [
+  {
+    id: 1,
+    value: "200+",
+    description: "International Brands",
+  },
+  {
+    id: 2,
+    value: "2000+",
+    description: "High-Quality Products",
+  },
+  {
+    id: 3,
+    value: "30000+",
+    description: "Happy Customers",
+  },
+];
+
+const BRAND_IMAGES = [
+  "brand1.png",
+  "brand2.png",
+  "brand3.png",
+  "brand4.png",
+  "brand5.png",
+];
 
 export default function Hero() {
   return (
     <section>
-      <div>
-        <div>
-          <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
+      <div className={styles["hero-container"]}>
+        <div className={styles["hero-text-container"]}>
+          <h1 className={styles["hero-text"]}>
+            FIND CLOTHES THAT MATCHES YOUR STYLE
+          </h1>
 
-          <p>
+          <p className={styles["hero-description"]}>
             Browse through our diverse range of meticulously crafted garments,
             designed to bring out your individuality and cater to your sense of
             style.
           </p>
 
-          <button>Shop Now</button>
+          <button type="button" className={styles["hero-button"]}>
+            Shop Now
+          </button>
 
-          <div>
-            <div>
-              <h3>200+</h3>
-              <p>International Brands</p>
-            </div>
+          <div className={styles["hero-stats"]}>
+            {HERO_STATS.map((item, index) => (
+              <>
+                <div className={styles["hero-stats-item"]}>
+                  <h3 className={styles["hero-stats-item-value"]}>
+                    {item.value}
+                  </h3>
+                  <p className={styles["hero-stats-item-description"]}>
+                    {item.description}
+                  </p>
+                </div>
 
-            <div>
-              <h3>2000+</h3>
-              <p>High-Quality Products</p>
-            </div>
-
-            <div>
-              <h3>30000+</h3>
-              <p>Happy Customers</p>
-            </div>
+                {index < HERO_STATS.length - 1 && (
+                  <span className={styles["hero-stats-separator"]}></span>
+                )}
+              </>
+            ))}
           </div>
         </div>
 
-        <div>
+        <div className={styles["hero-image-container"]}>
           <img
             src="/images/hero/hero.jpg"
             alt="Hero Image"
@@ -45,14 +75,9 @@ export default function Hero() {
         </div>
       </div>
 
-      <div className={styles["bg-black"]}>
+      <div className={styles["hero-brands"]}>
         {BRAND_IMAGES.map((item, i) => (
-          <img
-            key={i}
-            src={`/images/hero/${item}`}
-            alt="Hero Image"
-            width={100}
-          />
+          <img key={i} src={`/images/hero/${item}`} alt="Hero Image" />
         ))}
       </div>
     </section>
